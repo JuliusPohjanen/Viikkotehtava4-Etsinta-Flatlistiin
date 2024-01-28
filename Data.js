e81415c7d272862789e48f30e805e00ef222457e
PohjanenJulius <julius.pohjanen@gmail.com>
2024-01-28 15:53:42 +0200

    Tehtävä valmis :+1:
    jeps

---
+++ Data.js
@@ -0,0 +1,12 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'Jouni',
+    },
+    {
+        id: '2',
+        lastname: 'Ojala',
+        firstname: 'Kaisa',
+    },
+];
